bf9a0354d357037916c84dc2fbc00866efa9c44f
Raj Shekhar Sinha <raj18110sinha@gmail.com>
2024-10-22 22:35:57 +0530

    upload backend

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
+const ProductRouter = require('./Routes/ProductRouter');
+const router =require('./Routes/router');
+
+require('dotenv').config();
+require('./db/conn');
+require('./Models/db');
+const PORT = process.env.PORT || 8080;
+
+app.get('/ping', (req, res) => {
+    res.send('PONG');
+});
+
+app.use(bodyParser.json());
+app.use(cors({
+
+    origin: "https://employee-management-app-backend-4sp3.onrender.com"
+}
+));
+
+
+app.use(express.json());
+app.use("/uploads",express.static("./uploads"));
+app.use(router);
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
